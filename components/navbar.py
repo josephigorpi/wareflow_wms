75f919e54058d4332c753d41212f0047db08975d
josephigorpi <131630561+josephigorpi@users.noreply.github.com>
2026-06-10 00:02:21 -0500

    Update navbar.py

--- components/navbar.py
+++ components/navbar.py
@@ -7,19 +7,10 @@ from components.styles import load_global_styles
 
 def render_navbar(titulo: str, subtitulo: str = "", icono: str = "") -> None:
     load_global_styles()
-
+ 
+    subtitulo_html = f'<p class="navbar-subtitle">{subtitulo}</p>' if subtitulo else ""
     st.markdown(
-        f"""
-        <div class="navbar">
-            <div class="navbar-body">
-                <span class="navbar-icon">{icono}</span>
-                <div>
-                    <h1 class="navbar-title">{titulo}</h1>
-                    {f'<p class="navbar-subtitle">{subtitulo}</p>' if subtitulo else ''}
-                </div>
-            </div>
-        </div>
-        """,
+        f'<div class="navbar"><div class="navbar-body"><span class="navbar-icon">{icono}</span><div><h1 class="navbar-title">{titulo}</h1>{subtitulo_html}</div></div></div>',
         unsafe_allow_html=True,
     )
     st.markdown("<hr class='navbar-divider' />", unsafe_allow_html=True)
